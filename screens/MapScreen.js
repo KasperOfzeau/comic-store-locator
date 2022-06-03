c0bc5696394df64978f08c50fdbe529d73373e06
Kasper Beljaars <kasperbeljaars@hotmail.com>
2022-06-03 13:26:49 +0200

    Added pins from json file on map

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,27 +1,74 @@
-import React from "react";
-import { StyleSheet, Text, View, Dimensions } from 'react-native'
-import MapView, {Marker} from 'react-native-maps';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, Dimensions, Image } from 'react-native'
+import MapView, {Callout, Marker} from 'react-native-maps';
+import * as Location from 'expo-location';
+import jsonData from '../stores.json';
 import { Ionicons } from "@expo/vector-icons";
 
 const Map  = () => {
-return (
-	<View style={styles.container}>
-      <MapView 
-	  	style={styles.map}
-	    initialRegion={{
-			latitude: 37.78825,
-			longitude: -122.4324,
-			latitudeDelta: 0.0922,
-			longitudeDelta: 0.0421,
-		  }}>
-		  <Marker
-			coordinate={{latitude: 51.5905468101903, longitude: 4.782533728835702}}
-			title="De Stripspecialist"
-			description="076-5284700"
-		  />
-		</MapView>
-    </View>
-);
+
+	const [currentLocation, setCurrentLocation] = useState({
+		latitude: 0,
+		longitude: 0,
+	});
+
+	useEffect(() => {
+		(async () => {
+		  let { status } = await Location.requestForegroundPermissionsAsync();
+		  if (status !== 'granted') {
+			console.log('Permission to access location was denied');
+			return;
+		  }
+	
+		  let location = await Location.getCurrentPositionAsync({});
+		  if(location) {
+			setCurrentLocation({
+			  latitude: location.coords.latitude,
+			  longitude: location.coords.longitude
+		  	});
+		  }
+		})();
+	}, []);
+
+	return (
+		<View style={styles.container}>
+		<MapView 
+			style={styles.map}
+			region={{
+				latitude: currentLocation.latitude,
+				longitude: currentLocation.longitude,
+				latitudeDelta: 0.0922,
+				longitudeDelta: 0.0421,
+			}}
+			showsUserLocation={true}
+			>
+			{jsonData.map((prop, key) => {
+				// console.log(prop)
+         		return (
+					<Marker  
+						key={key}
+						coordinate={{latitude: parseFloat(prop.latitude), longitude: parseFloat(prop.longitude)}}
+						title= {prop.name}
+						description={prop.telephone}
+					>
+					<Image source={require('../assets/pin.png')} style={{height: 35, width:35 }} />
+					<Callout tooltip>
+						<View>
+							<View style={styles.bubble}>
+								<Text style={styles.name}>{prop.name}</Text>
+								<Text>Adres: {prop.adress}, {prop.city}</Text>
+								<Text>Telefoon: {prop.telephone}</Text>				
+							</View>
+							<View style={styles.arrowBorder}/>
+							<View style={styles.arrow}/>
+						</View>
+					</Callout>
+				 	</Marker>
+				);
+			})}
+			</MapView>
+		</View>
+	);
 };
 
 const styles = StyleSheet.create({
@@ -35,6 +82,36 @@ const styles = StyleSheet.create({
 	  width: Dimensions.get('window').width,
 	  height: Dimensions.get('window').height,
 	},
+	bubble: {
+		flexDirection: 'column',
+		alignSelf: 'flex-start',
+		backgroundColor: '#fff',
+		borderRadius: 6,
+		borderColor: "#ccc",
+		borderWidth: 0.5,
+		padding: 15,
+		width: 250,
+	},
+	arrow: {
+		backgroundColor: 'transparent',
+		borderColor: 'transparent',
+		borderTopColor: '#fff',
+		borderWidth: 16,
+		alignSelf: 'center',
+		marginTop: -32,
+	},
+	arrowBorder: {
+		backgroundColor: 'transparent',
+		borderColor: 'transparent',
+		borderTopColor: '#007a87',
+		borderWidth: 16,
+		alignSelf: 'center',
+		marginTop: -0.5,
+	},
+	name: {
+		fontSize: 16,
+		marginBottom: 5
+	}
   });
 
 export default Map;
