3f01e8f8c53d0f4b5f5cb4e8556dca14f2396514
Kasper Beljaars <kasperbeljaars@hotmail.com>
2022-06-03 15:09:31 +0200

    Display stores in list

--- screens/ListScreen.js
+++ screens/ListScreen.js
@@ -1,14 +1,42 @@
-import React from "react";
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View, Dimensions, Image,SafeAreaView, ScrollView, StatusBar } from 'react-native'
+import jsonData from '../stores.json';
 import { Ionicons } from "@expo/vector-icons";
 
-const List = () => {
-return (
-	<View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-	<Text style={{ color: "#006600", fontSize: 40 }}>List Screen!</Text>
-	<Ionicons name="list" size={80} color="#006600" />
-	</View>
-);
+const List  = () => {
+	return (
+      	<ScrollView>
+			  <View style={styles.container}>
+				{jsonData.map((prop, key) => {
+					// console.log(prop)
+					return (
+						<View key={key} style={styles.card}>
+							<Text>{prop.name}</Text>
+							<Text>Adres: {prop.adress}, {prop.city}</Text>
+							<Text>Telefoon: {prop.telephone}</Text>				
+						</View>
+					);
+				})}
+			</View>
+		</ScrollView>
+	);
 };
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+	    alignItems: 'center',
+	    justifyContent: 'center',
+		marginBottom: 20
+	  },
+	  card: {
+		  backgroundColor: '#fff',
+		  borderRadius: 6,
+		  borderColor: "#ccc",
+		  borderWidth: 0.5,
+		  padding: 15,
+		  width: '80%',
+		  marginTop: 15 
+	  }
+  });
 
 export default List;
