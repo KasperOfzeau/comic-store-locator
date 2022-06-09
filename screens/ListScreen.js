ae1f1835a4a6d45a0940964d0d83e144ecbc6e0c
Kasper Beljaars <kasperbeljaars@hotmail.com>
2022-06-09 13:30:52 +0200

    Added rating stars

--- screens/ListScreen.js
+++ screens/ListScreen.js
@@ -1,8 +1,18 @@
+import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View, Dimensions, Image,SafeAreaView, ScrollView, StatusBar } from 'react-native'
 import jsonData from '../stores.json';
+import StarRating from 'react-native-star-rating';
 import { Ionicons } from "@expo/vector-icons";
 
 const List  = () => {
+
+	const [ratings, setRatings] = useState({});
+
+	function onStarRatingPress(id, rating) {
+		setRatings({ ...ratings, [id] : rating })
+		console.log(ratings)
+	}
+
 	return (
       	<ScrollView>
 			  <View style={styles.container}>
@@ -12,7 +22,13 @@ const List  = () => {
 						<View key={key} style={styles.card}>
 							<Text>{prop.name}</Text>
 							<Text>Adres: {prop.adress}, {prop.city}</Text>
-							<Text>Telefoon: {prop.telephone}</Text>				
+							<Text>Telefoon: {prop.telephone}</Text>
+							<StarRating
+								disabled={false}
+								maxStars={5}
+								rating={ratings.hasOwnProperty(prop.name) ? ratings[prop.name] : 0}
+								selectedStar={(rating) => onStarRatingPress(prop.name, rating)}
+							/>
 						</View>
 					);
 				})}
